10a8e0ca9a22d4a7d7d47de3b134a4bb4206d423
Dmitry <d.a.demkin@gmail.com>
2025-06-24 15:56:54 +0500

    тестирование закончено

--- dictionary_test.go
+++ dictionary_test.go
@@ -1,1 +1,29 @@
 package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	d := Dictionary{
+		words: make(map[string]string),
+	}
+	word, meaning := "Рука", "Одна из конечностей"
+	d.Add(word, meaning)
+	meanResult, availability := d.words[word]
+	if !availability {
+		t.Errorf("Add(%s, %s) не добавил слово в словарь", word, meanResult)
+	} else if meanResult != meaning {
+		t.Errorf("Add(%s, %s) не корректно добавил слово в словарь", word, meanResult)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	word, meaning := "Рука", "Одна из конечностей"
+	d := Dictionary{
+		words: map[string]string{word: meaning},
+	}
+	d.Delete(word)
+	_, availability := d.words[word]
+	if availability == true {
+		t.Errorf("Deleate(%s) не удалило слово %s", word, word)
+	}
+}
